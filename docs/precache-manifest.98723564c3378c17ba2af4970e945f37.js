0d5c4901ad42c8fbf601ed3541aaec5f00af3933
Lebedev Konstantin <ibnrubaxa@gmail.com>
2020-05-21 16:31:51 +0300

    * host_rc by default

---
+++ docs/precache-manifest.98723564c3378c17ba2af4970e945f37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03350eac06cb6554c506810ccc92a91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed1c93b81c66979e19f6",
+    "url": "/static/css/2.4bc74a43.chunk.css"
+  },
+  {
+    "revision": "b13c4841b7ef27a971cf",
+    "url": "/static/css/main.115f7905.chunk.css"
+  },
+  {
+    "revision": "ed1c93b81c66979e19f6",
+    "url": "/static/js/2.375bc147.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.375bc147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13c4841b7ef27a971cf",
+    "url": "/static/js/main.d481cc68.chunk.js"
+  },
+  {
+    "revision": "91f679472a60c54fffa6",
+    "url": "/static/js/runtime-main.22dad6d7.js"
+  }
+]);
