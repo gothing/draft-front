a9c88fd07781e610439cefafe4fe51646df9ce7b
Lebedev Konstantin <ibnrubaxa@gmail.com>
2020-10-22 14:12:26 +0300

    feat: + support found status III

---
+++ docs/precache-manifest.8e2f1c93eb294bc3758bdead09f4cd1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fae0c846115c8962f9fc254ab465da3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47e004bef696f2b25833",
+    "url": "/static/css/2.790fa703.chunk.css"
+  },
+  {
+    "revision": "90168b5a3db0a7ff6f91",
+    "url": "/static/css/main.2d4ff133.chunk.css"
+  },
+  {
+    "revision": "47e004bef696f2b25833",
+    "url": "/static/js/2.1c9e948d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1c9e948d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90168b5a3db0a7ff6f91",
+    "url": "/static/js/main.4d18488d.chunk.js"
+  },
+  {
+    "revision": "91f679472a60c54fffa6",
+    "url": "/static/js/runtime-main.22dad6d7.js"
+  }
+]);
